79459322fb0375d438371de4516fc6ac4585f302
Nick Fomenko <dev.jesseme@gmail.com>
2024-04-12 14:19:22 +0300

    build: add auth.js integration with db (prisma)

--- src/shared/configs/auth.ts
+++ src/shared/configs/auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth"
+import { PrismaAdapter } from "@auth/prisma-adapter"
 import github from "next-auth/providers/github"
- 
+import { prisma } from "./prisma"
+
 export const { handlers, signIn, signOut, auth } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  session: {strategy: "jwt"},
   providers: [github],
 })
