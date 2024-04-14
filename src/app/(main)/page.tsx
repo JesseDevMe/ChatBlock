85b0c1477d53cfac7dd88502b1b006a05d56cf95
Nick Fomenko <dev.jesseme@gmail.com>
2024-04-14 18:41:27 +0300

    feat(sidebar): add menu badge

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,7 +1,12 @@
+import Menu from "@/features/menu/ui/menu";
+
 export default async function Home() {
   
   return (
-    <main className="p-5">
+    <main className="p-5 pb-0 h-screen bg-muted ">
+      <div className="flex flex-col gap-y-5 w-fit h-full bg-gray-400">
+        <Menu/>
+      </div>
       
     </main>
   );
