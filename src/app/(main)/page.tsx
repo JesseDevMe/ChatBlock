fdc80fea0eeadc232fb13ddd7dd4693072bb9538
Nick Fomenko <dev.jesseme@gmail.com>
2024-04-18 14:10:34 +0300

    feat: add menu option list

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -5,7 +5,7 @@ export default async function Home() {
   
   return (
     <main className="p-5 pb-0 h-screen bg-muted ">
-      <div className="flex flex-col gap-y-5 w-fit h-full bg-gray-400">
+      <div className="flex flex-col gap-y-5 w-fit h-full">
         <Menu/>
         <ChatsList/>
       </div>
